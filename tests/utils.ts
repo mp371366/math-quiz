d63db142a9cbc9cdfd3adf19c518d66b91f122a2
mp371366 <mp371366@github.com>
2020-09-02 23:06:25 +0200

    Fix tests.

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,12 +1,12 @@
 import { driver } from 'mocha-webdriver';
-import { User } from '../scripts/types';
+import User from '../scripts/types/user';
 
 export const BASE_URL = 'http://localhost:3000';
-export const LOGIN_URL = `${BASE_URL}/login`;
-export const CHANGE_PASSWORD_URL = `${BASE_URL}/change-password`;
+export const USER_URL = `${BASE_URL}/user`;
+export const CHANGE_PASSWORD_URL = `${USER_URL}/change-password`;
 
 export async function login({ username, password }: User = { username: 'user1', password: 'user1' }) {
-  await driver.get(LOGIN_URL);
+  await driver.get(USER_URL);
   await driver.find('input[name="username"').sendKeys(username);
   await driver.find('input[name="password"').sendKeys(password);
   await driver.find('#login-button').doClick();
